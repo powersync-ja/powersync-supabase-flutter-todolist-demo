b1cdf7d714d821fd490c4eb5537fe728b90e6efa
DominicGBauer <dominic@nomanini.com>
2023-11-20 13:45:29 +0200

    chore: add comments to fts helper

--- lib/fts_helpers.dart
+++ lib/fts_helpers.dart
@@ -2,10 +2,14 @@ import 'package:powersync_flutter_demo/powersync.dart';
 
 class FtsHelpers {
   static String _createSearchTermWithOptions(String searchTerm) {
+    // adding * to the end of the search term will match any word that starts with the search term
+    // e.g. searching bl will match blue, black, etc.
+    // consult FTS5 Full-text Query Syntax documentation for more options
     String searchTermWithOptions = '$searchTerm*';
     return searchTermWithOptions;
   }
 
+  /// Search the FTS table for the given searchTerm
   static Future<List> search(String searchTerm, String tableName) async {
     String searchTermWithOptions = _createSearchTermWithOptions(searchTerm);
     return await db.execute(
